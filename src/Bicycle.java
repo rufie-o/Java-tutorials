635cee8846e6b8bd09df5d289962a689919b660d
rufie-o <35864170+rufie-o@users.noreply.github.com>
2020-12-15 16:30:46 +0100

    Update

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -3,6 +3,7 @@ interface IntBicycle {
     // wheel revolutions per minute
     void changeCadence(int newValue);
 
+    // selected gear
     void changeGear(int newValue);
 
     void speedUp(int increment);
