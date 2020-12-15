627707cb979ce2c16af039045315d22c3e081f9e
rufie-o <35864170+rufie-o@users.noreply.github.com>
2020-12-15 14:17:34 +0100

    Add files via upload
    Initial files

---
+++ Bicycle.java
@@ -0,0 +1,46 @@
+interface IntBicycle {
+
+    // wheel revolutions per minute
+    void changeCadence(int newValue);
+
+    void changeGear(int newValue);
+
+    void speedUp(int increment);
+
+    void applyBrakes(int decrement);
+}
+
+public class Bicycle implements IntBicycle {
+
+    int cadence = 0;
+    int speed = 0;
+    int gear = 1;
+    String type = "Bike";
+
+    public void changeCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    public void changeGear(int newValue) {
+        gear = newValue;
+    }
+
+    public void speedUp(int increment) {
+        speed = speed + increment;
+    }
+
+    public void applyBrakes(int decrement) {
+        speed = speed - decrement;
+    }
+
+    void printStates() {
+        System.out.println("cadence:" + cadence + " speed:" + speed + " gear:" + gear);
+    }
+}
+
+class MountainBike extends Bicycle {
+
+    // new fields and methods defining
+    // a mountain bike would go here
+    String type = "Mountain bike";
+}
